e1363eb8ef6508c0061887c1e187e678a5ef543c
jaejoong0529 <wcn07003@naver.com>
2025-03-03 17:31:40 +0900

    refactor: 상품 등록 쿠키저장방식

--- src/main/java/kjj/articket2/product/controller/ProductController.java
+++ src/main/java/kjj/articket2/product/controller/ProductController.java
@@ -1,15 +1,15 @@
 package kjj.articket2.product.controller;
 
+import kjj.articket2.global.jwt.JwtUtil;
 import kjj.articket2.member.domain.Member;
+import kjj.articket2.member.repository.MemberRepository;
 import kjj.articket2.product.dto.ProductCreateRequest;
 import kjj.articket2.product.dto.ProductCreateResponse;
 import kjj.articket2.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +17,16 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/create")
-    public ResponseEntity<ProductCreateResponse> createProduct(@RequestBody ProductCreateRequest request,
-                                                               @SessionAttribute("member") Member member) {
-        ProductCreateResponse response = productService.createProduct(request,member);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> createProduct(@RequestBody ProductCreateRequest request,
+                                                               @CookieValue(value = "accessToken", required = false)
+                                                               String accessToken) {
+        productService.createProduct(request, accessToken);
+        return ResponseEntity.status(HttpStatus.CREATED).body("상품이 성공적으로 등록되었습니다.");
+
     }
+//    public ResponseEntity<ProductCreateResponse> createProduct(@RequestBody ProductCreateRequest request,
+//                                                               @SessionAttribute("member") Member member) {
+//        ProductCreateResponse response = productService.createProduct(request,member);
+//        return ResponseEntity.ok(response);
+//    }
 }
--- src/main/java/kjj/articket2/product/service/ProductService.java
+++ src/main/java/kjj/articket2/product/service/ProductService.java
@@ -1,7 +1,9 @@
 package kjj.articket2.product.service;
 
+import kjj.articket2.global.jwt.JwtUtil;
 import kjj.articket2.member.MemberConverter;
 import kjj.articket2.member.domain.Member;
+import kjj.articket2.member.exception.MemberNotFoundException;
 import kjj.articket2.member.repository.MemberRepository;
 import kjj.articket2.product.ProductConverter;
 import kjj.articket2.product.domain.Product;
@@ -18,15 +20,24 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Transactional
 public class ProductService {
-    private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
 
-    public ProductCreateResponse createProduct(ProductCreateRequest request,Member member) {
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
+    public void createProduct(ProductCreateRequest request, String accessToken) {
+        if (accessToken == null || !jwtUtil.validateToken(accessToken)) {
+            throw new RuntimeException("허용되지 않았습니다");
+        }
+
+        String username = jwtUtil.getUsernameFromToken(accessToken);
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new MemberNotFoundException("사용자를 찾을 수 없습니다."));
+
         Product product = ProductConverter.fromDto(request, member).toBuilder()
                 .createdAt(LocalDateTime.now())
                 .build();
         productRepository.save(product);
-        return ProductConverter.toCreateProductResponse(product);
-
     }
 }
+
