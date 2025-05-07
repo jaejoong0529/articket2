ccedaff9bf59912cebf8ed73ff16e6c81a0a4541
jaejoong0529 <wcn07003@naver.com>
2025-05-07 23:18:33 +0900

    style: 메인 화면 UI,CSS 수정

--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -1,38 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../css/Home.css';
 
 function Home() {
     const navigate = useNavigate();
 
-    const handleButtonClick = () => {
-        navigate('/login');
-    };
-
-    const handleSignupClick = () => {
-        navigate('/signup');
-    };
-
-    const handleFindUsernameClick = () => {
-        navigate('/findUsername');
-    };
-
-    const handleFindPasswordClick = () => {
-        navigate('/findPassword');
-    };
-
     return (
-        <div>
-            <h1>Welcome to Articket2!</h1>
-            <p>
-                Articket2
-            </p>
-            <div>
-                <button onClick={handleButtonClick}>로그인</button>
-                <button onClick={handleSignupClick}>회원가입</button>
-                <button onClick={handleFindUsernameClick}>아이디 찾기</button>
-                <button onClick={handleFindPasswordClick}>비밀번호 찾기</button>
-            </div>
-            {/* 추가적인 기능들 (인기 공연 목록, 검색 등) */}
+        <div className="home-container">
+            <header className="home-header">
+                <div className="logo" onClick={() => window.location.reload()}>Articket</div>
+            </header>
+
+            <main className="main-content">
+                <h1 className="main-title">Articket에서 중고 거래 경매 하세요!</h1>
+                <div className="search-box">
+                    <input type="text" placeholder="검색어를 입력해주세요" />
+                    <button className="search-btn">🔍</button>
+                </div>
+                <div className="button-group">
+                    <button onClick={() => navigate('/login')}>로그인</button>
+                    <button onClick={() => navigate('/signup')}>회원가입</button>
+                    <button onClick={() => navigate('/findUsername')}>아이디 찾기</button>
+                    <button onClick={() => navigate('/findPassword')}>비밀번호 찾기</button>
+                </div>
+            </main>
         </div>
     );
 }
