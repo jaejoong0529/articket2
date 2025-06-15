7478254501bf174e7d74ed4f7b19f91407d3da75
jaejoong0529 <wcn07003@naver.com>
2025-06-15 22:29:49 +0900

    refactor: RequestMapping 수정

--- src/main/java/kjj/articket2/auth/controller/AuthController.java
+++ src/main/java/kjj/articket2/auth/controller/AuthController.java
@@ -9,13 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     private final AuthService authService;
+
     //회원가입
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(@Valid @RequestBody MemberSignUpRequest request) {
