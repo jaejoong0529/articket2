4b35c0414d1caf77d778d5c5e61d154e508cb7d6
jaejoong0529 <wcn07003@naver.com>
2025-06-15 22:39:30 +0900

    refactor: @Valid 추가

--- src/main/java/kjj/articket2/member/controller/MemberController.java
+++ src/main/java/kjj/articket2/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package kjj.articket2.member.controller;
 
+import jakarta.validation.Valid;
 import kjj.articket2.global.jwt.CustomUserDetails;
 import kjj.articket2.member.MemberConverter;
 import kjj.articket2.member.dto.*;
@@ -16,25 +17,25 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberConverter memberConverter;
     @PostMapping("/findUsername")
-    public ResponseEntity<String> findUsername(@RequestBody MemberFindUsernameRequest request) {
+    public ResponseEntity<String> findUsername(@Valid @RequestBody MemberFindUsernameRequest request) {
         memberService.findUsername(request);
         return ResponseEntity.ok("아이디가 이메일로 전송되었습니다.");
     }
 
     @PostMapping("/findPassword")
-    public ResponseEntity<String> findPassword(@RequestBody MemberFindPasswordRequest request) {
+    public ResponseEntity<String> findPassword(@Valid @RequestBody MemberFindPasswordRequest request) {
         memberService.findPassword(request);
         return ResponseEntity.ok("임시 비밀번호가 이메일로 전송되었습니다.");
     }
 
     @PostMapping("/changePassword")
-    public ResponseEntity<String> changePassword(@RequestBody MemberChangePasswordRequest request) {
+    public ResponseEntity<String> changePassword(@Valid @RequestBody MemberChangePasswordRequest request) {
         memberService.changePassword(request);
         return ResponseEntity.ok("비밀번호가 변경되었습니다.");
     }
 
     @PostMapping("/recharge")
-    public ResponseEntity<String> rechargeMoney(@RequestBody MoneyRechargeRequest request,
+    public ResponseEntity<String> rechargeMoney(@Valid @RequestBody MoneyRechargeRequest request,
                                                 @AuthenticationPrincipal CustomUserDetails userDetails) {
         memberService.rechargeMoney(request, userDetails);
         return ResponseEntity.ok("충전이 완료되었습니다.");
