c1086d7e79f78917679a111002ba19060212f406
jaejoong0529 <wcn07003@naver.com>
2025-06-25 18:34:55 +0900

    fix: RequestMapping 경로 수정 및 상품 DTO 유효성 검사(@Valid) 추가

--- src/main/java/kjj/articket2/product/controller/ProductController.java
+++ src/main/java/kjj/articket2/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package kjj.articket2.product.controller;
 
+import jakarta.validation.Valid;
 import kjj.articket2.global.jwt.CustomUserDetails;
 import kjj.articket2.product.domain.Category;
 import kjj.articket2.product.dto.ProductCreateRequest;
@@ -16,17 +17,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000") // React와 연결
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/product")
+@RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
 
     //상품등록
     @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String> createProduct(
-            @RequestPart("request") ProductCreateRequest request,
+            @Valid @RequestPart("request") ProductCreateRequest request,
             @RequestPart(value = "image", required = false) MultipartFile image,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
         productService.createProduct(request, image, userDetails);
@@ -65,9 +65,10 @@ public class ProductController {
     // 상품 수정(판매자만 가능)
     @PutMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String> updateProduct(@PathVariable Long id,
-                                                @RequestPart("request") ProductUpdateRequest request,
+                                                @Valid @RequestPart("request") ProductUpdateRequest request,
                                                 @RequestPart(value = "image", required = false) MultipartFile image,
                                                 @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         productService.updateProduct(id, request, image, userDetails);
         return ResponseEntity.ok("상품이 수정되었습니다.");
     }
