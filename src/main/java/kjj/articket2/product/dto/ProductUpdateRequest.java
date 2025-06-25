97b9755410a8a28f37ff7ecb66488317981fb3e7
jaejoong0529 <wcn07003@naver.com>
2025-06-25 18:37:21 +0900

    feat: 상품 요청 DTO에 유효성 검사 어노테이션 추가

--- src/main/java/kjj/articket2/product/dto/ProductUpdateRequest.java
+++ src/main/java/kjj/articket2/product/dto/ProductUpdateRequest.java
@@ -1,5 +1,9 @@
 package kjj.articket2.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import kjj.articket2.product.domain.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,20 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductUpdateRequest {
+    @NotBlank(message = "상품명을 입력해주세요.")
     private String productName;
-    private String description;
-    private Integer price;
-}
 
+    @NotBlank(message = "상품 설명을 입력해주세요.")
+    private String description;
+
+    @NotNull(message = "가격은 필수입니다.")
+    @Positive(message = "가격은 0보다 커야 합니다.")
+    private Integer price;
+
+    @NotNull(message = "즉시구매가는 필수입니다.")
+    @Positive(message = "즉시구매가는 0보다 커야 합니다.")
+    private Integer buyNowPrice;
+
+    @NotNull(message = "카테고리를 선택해주세요.")
+    private Category category;
+}
