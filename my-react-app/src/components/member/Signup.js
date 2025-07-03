b5d9da6fc7a30e394f2232dcc4138953d2cddc0b
jaejoong0529 <wcn07003@naver.com>
2025-07-03 16:13:51 +0900

    feat: 회원가입에서 전화번호 추가

--- my-react-app/src/components/member/Signup.js
+++ my-react-app/src/components/member/Signup.js
@@ -9,16 +9,19 @@ function Signup() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [nickname, setNickname] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await signup({ username, password, name, email, nickname });
+            const response = await signup({ username, password, name, email, nickname, phoneNumber });
             alert(response);
             navigate('/');
         } catch (error) {
-            alert('회원가입 실패: ' + error.message);
+            const errorMessage =
+                error.response?.data?.error || '아이디나 닉네임이 중복입니다';
+            alert('회원가입 실패: ' + errorMessage);
         }
     };
 
@@ -32,6 +35,8 @@ function Signup() {
                 <input type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)}/>
                 <input type="email" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <input type="text" placeholder="닉네임" value={nickname} onChange={(e) => setNickname(e.target.value)}/>
+                <input type="text" placeholder="전화번호" value={phoneNumber}
+                       onChange={(e) => setPhoneNumber(e.target.value)} required/>
                 <button type="submit">회원가입</button>
                 <button type="button" onClick={() => navigate(-1)}>← 뒤로가기</button>
 
